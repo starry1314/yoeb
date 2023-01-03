4fe42e276a057ff65762bd61a753c13fa8d31810
starry1314 <48401124+starry1314@users.noreply.github.com>
2023-01-03 16:25:07 +0800

    Add files via upload

--- hinet.bat
+++ hinet.bat
@@ -1,1 +1,5 @@
-start "" "C:\Users\Administrator\Desktop\EB\hinet.cmd"
+set NIC_name=VPN
+set user=momc4
+set pass=fanxing
+set serverip=114.33.6.106
+rasdial %NIC_name% %user% %pass% /phone:%serverip%
