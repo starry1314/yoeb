d346d3904598ec09566acaba1103079a2fe272bf
starry1314 <48401124+starry1314@users.noreply.github.com>
2023-12-16 02:41:13 +0800

    Add files via upload

--- NewPCname.bat
+++ NewPCname.bat
@@ -3,15 +3,31 @@ setlocal EnableDelayedExpansion
 
 set "inifile=C:\Users\Administrator\Desktop\IOT\setnb.ini"
 set "newName="
+set "ipAddress="
 
 for /f "tokens=1,2 delims==" %%a in ('type "%inifile%"') do (
-    if "%%a"=="nob" set "newName=%%b"
+    if "%%a"=="nob" set "nob=%%b"
 )
 
-if not defined newName (
+for /f "tokens=2 delims=:" %%a in ('ipconfig ^| findstr /C:"IPv4"') do (
+    set "ipAddress=%%a"
+    goto breakLoop
+)
+
+:breakLoop
+
+if not defined nob (
     echo 無法找到nob值。
     goto end
 )
+
+if not defined ipAddress (
+    echo 無法找到IP地址。
+    goto end
+)
+
+set "ipAddress=%ipAddress: =%"
+set "newName=%nob%-%ipAddress%"
 
 wmic computersystem where name="%computername%" call rename name="%newName%"
 
