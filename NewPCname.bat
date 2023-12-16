82a3e4a551fcf7ed620ecc4aad267db88fbb21a0
starry1314 <48401124+starry1314@users.noreply.github.com>
2023-12-16 15:11:32 +0800

    Update NewPCname.bat

--- NewPCname.bat
+++ NewPCname.bat
@@ -1,5 +1,11 @@
 @echo off
+PUSHD %~DP0 & cd /d "%~dp0"
+%1 %2
+mshta vbscript:createobject("shell.application").shellexecute("%~s0","goto :runas","","runas",1)(window.close)&goto :eof
+:runas
+
 setlocal EnableDelayedExpansion
+
 
 set "inifile=C:\Users\Administrator\Desktop\IOT\setnb.ini"
 set "newName="
